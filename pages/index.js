31abe9725757a417399ae8cf07e7d6a1847872f9
Georg Jaehnig <georg@jaehnig.org>
2019-06-17 16:34:25 +0200

    Demo code for react-swipy.

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,54 @@
-export default function Index() {
-  return (
-    <div>
-      <p>Hello Next.js</p>
-    </div>
-  )
+import React, {Component} from "react";
+import Swipeable from "react-swipy"
+
+import Card from "./components/Card";
+import Button from "./components/Button";
+
+const wrapperStyles = {position: "relative", width: "250px", height: "250px"};
+const actionsStyles = {
+  display: "flex",
+  justifyContent: "space-between",
+  marginTop: 12,
+};
+
+class App extends Component {
+  state = {
+    cards: ["First", "Second", "Third"],
+  };
+
+  remove = () =>
+    this.setState(({cards}) => ({
+      cards: cards.slice(1, cards.length),
+    }));
+
+  render() {
+    const {cards} = this.state;
+
+    return (
+      <div>
+        <div style={wrapperStyles}>
+          {cards.length > 0 ? (
+            <div style={wrapperStyles}>
+              <Swipeable
+                buttons={({left, right}) => (
+                  <div style={actionsStyles}>
+                    <Button onClick={left}>Reject</Button>
+                    <Button onClick={right}>Accept</Button>
+                  </div>
+                )}
+                onAfterSwipe={this.remove}
+              >
+                <Card>{cards[0]}</Card>
+              </Swipeable>
+              {cards.length > 1 && <Card zIndex={-1}>{cards[1]}</Card>}
+            </div>
+          ) : (
+            <Card zIndex={-2}>No more cards</Card>
+          )}
+        </div>
+      </div>
+    );
+  }
 }
+
+export default App;
