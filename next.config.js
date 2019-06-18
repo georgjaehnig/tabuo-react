c6515437e1bcbf7ad92827ac22bddd1442657778
Georg Jaehnig <georg@jaehnig.org>
2019-06-18 19:51:56 +0200

    Config next-sass.

---
+++ next.config.js
@@ -0,0 +1,11 @@
+const withPlugins = require("next-compose-plugins");
+const withSass = require("@zeit/next-sass");
+
+module.exports = withPlugins([
+  withSass,
+  {
+    sassLoaderOptions: {
+      includePaths: ["src/"]
+    }
+  }
+]);
