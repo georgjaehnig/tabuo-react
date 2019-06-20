0eba9059cad6ffdf7c30b9b11c9bc036e0813fb9
Georg Jaehnig <georg@jaehnig.org>
2019-06-20 19:16:53 +0200

    Remove unused imports.

--- pages/components/Button.js
+++ pages/components/Button.js
@@ -1,5 +1,3 @@
-import React from "react";
-
 const buttonStyles = {
   padding: "16px 24px",
   background: "whitesmoke",
--- pages/components/Card.js
+++ pages/components/Card.js
@@ -1,5 +1,3 @@
-import React from "react";
-
 const cardStyles = {
   background: "whitesmoke",
   borderRadius: 3,
