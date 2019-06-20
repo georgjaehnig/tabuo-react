22019bb57baa6743b1be99039af5021ab8c037bd
Georg Jaehnig <georg@jaehnig.org>
2019-06-20 21:10:54 +0200

    Use cardStyles and show "No more cards.".

--- pages/components/TabuoCard.js
+++ pages/components/TabuoCard.js
@@ -1,17 +1,35 @@
 import { Card, List } from 'rbx';
 
+const cardStyles = {
+  cursor: "pointer",
+  userSelect: "none",
+  position: "absolute",
+  top: 50,
+  left: 40,
+  width: 300,
+  heigth: 500,
+};
+
 const TabuoCard = ({ zIndex = 0, card }) => (
-  <div style={{ ...zIndex }}>
-    <Card>
-      <Card.Header>
-        <Card.Header.Title align="centered">{card.target}</Card.Header.Title>
-      </Card.Header>
-      <Card.Content textAlign="centered">
-        <List as="ul">
-          {card.taboos.map((item, key) => <List.Item as="li" key={key}>{item}</List.Item> )}
-        </List>
-      </Card.Content>
-    </Card>
+  <div style={{ ...cardStyles, zIndex }}>
+    { card ? (
+      <Card>
+        <Card.Header>
+          <Card.Header.Title align="centered">{card.target}</Card.Header.Title>
+        </Card.Header>
+        <Card.Content textAlign="centered" >
+          <List as="ul">
+            {card.taboos.map((item, key) => <List.Item as="li" key={key}>{item}</List.Item> )}
+          </List>
+        </Card.Content>
+      </Card>
+    ) : (
+      <Card>
+        <Card.Content textAlign="centered">
+          No more cards.
+        </Card.Content>
+      </Card>
+    )}
   </div>
 );
 
