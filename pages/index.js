debceec12ed85221dc7b61e4f7e31ecb7d6b0f15
Georg Jaehnig <georg@jaehnig.org>
2019-07-25 01:57:55 +0200

    Move button.

--- pages/index.js
+++ pages/index.js
@@ -185,8 +185,8 @@ class App extends Component {
         <Container>
           <Level breakpoint="mobile">
             <Level.Item textSize={3}><FontAwesomeIcon icon={faSmileWink} /> </Level.Item>
-            <Level.Item> <Button onClick={this.timerStartPause}>❚❚</Button> </Level.Item>
             <Level.Item>
+              <Button onClick={this.timerStartPause} style={{marginRight: '1em'}}>❚❚</Button>
               <TimerMachine
                 timeStart={60 * 1000}               // Start at 60 seconds.
                 timeEnd={0 * 1000} 
