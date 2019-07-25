285d77d9faf5aa8b5bfc76e59bf39ced54cde85b
Georg Jaehnig <georg@jaehnig.org>
2019-07-25 14:29:17 +0200

    Allow setting disabled.

--- pages/components/TabuoButton.js
+++ pages/components/TabuoButton.js
@@ -1,9 +1,9 @@
 import { Button } from 'rbx';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-const TabuoButton = ({ textColor, onClick, icon, counter }) => {
+const TabuoButton = ({ textColor, onClick, icon, counter, disabled }) => {
 
-  return <Button textColor={textColor} textSize={4} onClick={onClick} style={{ width:'70%' }}> 
+  return <Button textColor={textColor} textSize={4} onClick={onClick} style={{ width:'70%' }} disabled={disabled} > 
 			<FontAwesomeIcon icon={icon} /> 
 			<span>&nbsp;</span>
 			<div>{counter}</div>
