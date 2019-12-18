84492fb446a23550aae47221f197a7906639e144
Georg Jaehnig <georg@jaehnig.org>
2019-12-18 23:22:47 +0300

    Set backgrounds Header and Footer.

--- src/components/TabuoCard.js
+++ src/components/TabuoCard.js
@@ -15,12 +15,12 @@ const TabuoCard = ({ zIndex = 0, title, content, footer }) => {
   return <div style={{ ...cardStyles, zIndex }} >
     <Card textSize={4} style={{height: '65vh'}}>
       <Card.Header>
-        <Card.Header.Title align="centered">{title}</Card.Header.Title>
+        <Card.Header.Title align="centered" style={{background: '#1a86ce'}} textColor='white'>{title}</Card.Header.Title>
       </Card.Header>
       <Card.Content textAlign="centered">{content}</Card.Content>
       {footer && 
         <Card.Footer>
-          <Card.Footer.Item align="centered" textSize={6} style={{fontStyle:'italic'}}>{footer}</Card.Footer.Item>
+          <Card.Footer.Item align="centered" textSize={6} style={{fontStyle:'italic', background: 'lightgrey'}}>{footer}</Card.Footer.Item>
         </Card.Footer>
       }
     </Card>
