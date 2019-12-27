f6ffe752c66248a6e328b3f17f8601282fcbb16d
Georg Jaehnig <georg@jaehnig.org>
2019-12-27 01:56:23 +0100

    Styles into tag.

--- src/components/TabuoCard.js
+++ src/components/TabuoCard.js
@@ -1,41 +1,43 @@
 import React from "react";
 import { Card } from "rbx";
 
-const cardStyles = {
-  cursor: "pointer",
-  userSelect: "none",
-  position: "absolute",
-  top: 0,
-  left: "5%",
-  width: "90%",
-  height: "65vh",
-};
-
 const TabuoCard = ({ zIndex = 0, title, content, footer }) => {
   return (
-      <Card textSize={4} style={{ ...cardStyles, zIndex }} >
-        <Card.Header>
-          <Card.Header.Title
+    <Card
+      textSize={4}
+      style={{
+        cursor: "pointer",
+        userSelect: "none",
+        position: "absolute",
+        top: 0,
+        left: "5%",
+        width: "90%",
+        height: "65vh",
+        zIndex: zIndex,
+      }}
+    >
+      <Card.Header>
+        <Card.Header.Title
+          align="centered"
+          textColor="white"
+          style={{ background: "#1a86ce" }}
+        >
+          {title}
+        </Card.Header.Title>
+      </Card.Header>
+      <Card.Content textAlign="centered">{content}</Card.Content>
+      {footer && (
+        <Card.Footer>
+          <Card.Footer.Item
             align="centered"
-            textColor="white"
-            style={{ background: "#1a86ce" }}
+            textSize={6}
+            style={{ fontStyle: "italic", background: "lightgrey" }}
           >
-            {title}
-          </Card.Header.Title>
-        </Card.Header>
-        <Card.Content textAlign="centered">{content}</Card.Content>
-        {footer && (
-          <Card.Footer>
-            <Card.Footer.Item
-              align="centered"
-              textSize={6}
-              style={{ fontStyle: "italic", background: "lightgrey" }}
-            >
-              {footer}
-            </Card.Footer.Item>
-          </Card.Footer>
-        )}
-      </Card>
+            {footer}
+          </Card.Footer.Item>
+        </Card.Footer>
+      )}
+    </Card>
   );
 };
 
