b32b0ab97b15662fd03a8df62196b7981079e263
Georg Jaehnig <georg@jaehnig.org>
2019-12-27 01:50:37 +0100

    Rename.

--- src/components/TabuoCard.js
+++ src/components/TabuoCard.js
@@ -1,41 +1,41 @@
 import React from "react";
 import { Card } from "rbx";
 
-const cardStyles = {
+const styles = {
   cursor: "pointer",
   userSelect: "none",
   position: "absolute",
   top: 0,
   left: "5%",
   width: "90%",
-  height: "65vh"
+  height: "65vh",
 };
 
 const TabuoCard = ({ zIndex = 0, title, content, footer }) => {
   return (
-    <Card textSize={4} style={{ ...cardStyles, zIndex }}>
-      <Card.Header>
-        <Card.Header.Title
-          align="centered"
-          style={{ background: "#1a86ce" }}
-          textColor="white"
-        >
-          {title}
-        </Card.Header.Title>
-      </Card.Header>
-      <Card.Content textAlign="centered">{content}</Card.Content>
-      {footer && (
-        <Card.Footer>
-          <Card.Footer.Item
+      <Card textSize={4} style={{ ...styles, zIndex }} >
+        <Card.Header>
+          <Card.Header.Title
             align="centered"
-            textSize={6}
-            style={{ fontStyle: "italic", background: "lightgrey" }}
+            style={{ background: "#1a86ce" }}
+            textColor="white"
           >
-            {footer}
-          </Card.Footer.Item>
-        </Card.Footer>
-      )}
-    </Card>
+            {title}
+          </Card.Header.Title>
+        </Card.Header>
+        <Card.Content textAlign="centered">{content}</Card.Content>
+        {footer && (
+          <Card.Footer>
+            <Card.Footer.Item
+              align="centered"
+              textSize={6}
+              style={{ fontStyle: "italic", background: "lightgrey" }}
+            >
+              {footer}
+            </Card.Footer.Item>
+          </Card.Footer>
+        )}
+      </Card>
   );
 };
 
