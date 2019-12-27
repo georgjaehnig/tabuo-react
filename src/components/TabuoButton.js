585c8cf667aa817c59940bb18cb61c514b723670
Georg Jaehnig <georg@jaehnig.org>
2019-12-27 04:45:09 +0100

    Format.

--- src/components/TabuoButton.js
+++ src/components/TabuoButton.js
@@ -1,14 +1,20 @@
-import React from 'react';
-import { Button } from 'rbx';
+import React from "react";
+import { Button } from "rbx";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const TabuoButton = ({ textColor, onClick, icon, counter, disabled }) => {
-
-  return <Button textColor={textColor} onClick={onClick} style={{ width:'70%', fontSize: '4vh'}} disabled={disabled} > 
-			<FontAwesomeIcon icon={icon} /> 
-			<span>&nbsp;</span>
-			<div>{counter}</div>
-		</Button>
+  return (
+    <Button
+      textColor={textColor}
+      onClick={onClick}
+      style={{ width: "70%", fontSize: "4vh" }}
+      disabled={disabled}
+    >
+      <FontAwesomeIcon icon={icon} />
+      <span>&nbsp;</span>
+      <div>{counter}</div>
+    </Button>
+  );
 };
 
 export default TabuoButton;
